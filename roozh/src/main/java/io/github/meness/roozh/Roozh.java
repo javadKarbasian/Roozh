643536b736f826537fa51cec42cc4de0b6a4d492
Alireza <aesshoferi@gmail.com>
2016-07-04 04:24:38 +0430

    nothing

--- roozh/src/main/java/io/github/meness/roozh/Roozh.java
+++ roozh/src/main/java/io/github/meness/roozh/Roozh.java
@@ -24,7 +24,6 @@ import java.util.Calendar;
  *
  * @author Kaveh Shahbazian
  * @author Alireza Eskandarpour Shoferi
- * @version 2.0
  * @see <a href="https://github.com/meNESS/Roozh/">Roozh on Github</a>
  * @see <a href="http://www.astro.uni.torun.pl/~kb/Papers/EMP/PersianC-EMP.htm">The Persian calendar for 3000 years</a>
  * @since 0.0.1-alpha
